8bd2a9e9e9f13689d091db0bd6143863e7d20700
anaraquel <anaraquel00@gmail.com>
2025-09-29 16:47:08 -0300

    updates about home header footer add videos

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,12 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 @Component({
   selector: 'app-header',
-  imports: [RouterModule, MatToolbarModule, MatButtonModule, CommonModule],
+  imports: [RouterModule,
+       MatToolbarModule,
+       MatButtonModule,
+       CommonModule,
+       MatIconModule,
+       MatMenuModule
+  ],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
