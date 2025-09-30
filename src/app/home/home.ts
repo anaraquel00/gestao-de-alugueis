f60a46a1352b494b0c2e3bebca8975892083c995
anaraquel <anaraquel00@gmail.com>
2025-09-30 17:23:14 -0300

    update ortografia

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,14 +1,34 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
-
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, inject, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-home',
   imports: [RouterModule, CommonModule, MatButtonModule],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
-export class Home {
+export class Home implements AfterViewInit {
+  ngAfterViewInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  @ViewChild('bgVideo') videoElement!: ElementRef<HTMLVideoElement>;
+   // Injetamos o ChangeDetectorRef para um controle mais fino, se necessário
+  // Uma flag para garantir que só tentaremos dar play uma vez
+  private videoPlayed = false;
 
+  /**
+   * Esta função será chamada por qualquer clique na área do vídeo.
+   */
+  tryToPlayVideo(): void {
+    // Se o vídeo ainda não tocou e o elemento existe...
+    if (!this.videoPlayed && this.videoElement) {
+      this.videoPlayed = true; // Marca que já tentamos
+      this.videoElement.nativeElement.play().catch(error => {
+        // Se mesmo assim o navegador bloquear, apenas registramos no console.
+        console.warn('O navegador ainda bloqueou o play após a interação.', error);
+      });
+    }
+  }
 }
+
